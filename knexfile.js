d8469c21f807d4f3f207ba27ff5bfcefc59f8343
AlexanderTrue <alexdtrue@hotmail.com>
2023-05-26 14:30:51 -0600

    Updated with dotenv!

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const connectionString = process.env.DB_CONNECTION_STRING;
 // Update with your config settings.
 
 /**
@@ -6,7 +8,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://postgres:docker@localhost:5432/per_store",
+    connection: connectionString,
   },
 
   staging: {
